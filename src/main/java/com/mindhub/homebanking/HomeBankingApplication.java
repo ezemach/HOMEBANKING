eafe4e743bb4c0223d0c0f40103142ea9054e987
Ezequiel <ezemazzucco@gmail.com>
2023-07-25 18:01:49 -0300

    mart1

--- src/main/java/com/mindhub/homebanking/HomeBankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomeBankingApplication.java
@@ -60,7 +60,7 @@ public class HomeBankingApplication {
 
 			ClientLoan loan1 = new ClientLoan(650000,60,"Hipotecario" );
 			ClientLoan loan2 = new ClientLoan(550000,12,"Personal" );
-			ClientLoan loan3 = new ClientLoan(280000,24,"Personal");
+			ClientLoan loan3 = new ClientLoan(250000,24,"Personal");
 			ClientLoan loan4 = new ClientLoan(320000,36, "Automotriz");
 
 			client1.addClientLoan(loan1);
